cbef77bc6342374d6fc84b08ae05c32dcee82fae
djennedy <darrenjennedy@gmail.com>
2023-04-04 22:11:32 -0700

    Fixed gps logic

--- gpsTracker.c
+++ gpsTracker.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
+#include <stdio.h>
 #include <pthread.h>
 #include "gpsController.h"
 #include "gpsTracker.h"
@@ -12,7 +13,7 @@
 
 #define MY_PI (3.14159265358979323846264338327950288) // Value copied from https://c-for-dummies.com/blog/?p=3015
 
-#define DIST_THRESHOLD_m 3 
+#define DIST_THRESHOLD_m 5
 
 #define DEGREE_METER_ESTIMATE 111111
 
@@ -29,17 +30,20 @@ static double getDistanceBetweenLats(double lat1, double lat2) {
     // Formula used is a very rough approximate, taken from username whuber's comment on  https://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
     // Using an exact formula, such as haversine, will introduce rounding errors because the difference between our values are small
 
-    double dist = fabs(lat1 - lat2);
-    return dist * DEGREE_METER_ESTIMATE;
+    double lat1Meters = lat1 * DEGREE_METER_ESTIMATE;
+    double lat2Meters = lat2 * DEGREE_METER_ESTIMATE;
+    return fabs(lat1Meters - lat2Meters);
 }
 
-static double getDistanceBetweenLons(double lon1, double lon2) {
+static double getDistanceBetweenLons(double lat1, double lat2) {
     // Formula used is a very rough approximate, taken from username whuber's comment on  https://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
     // Using an exact formula, such as haversine, will introduce rounding errors because the difference between our values are small
 
-    double dist = fabs(lon1 - lon2);
-    double distRadians = dist * (MY_PI/180.0);
-    return cos(distRadians) * DEGREE_METER_ESTIMATE;
+    double lat1Radians = lat1 * (MY_PI/180);
+    double lat2Radians = lat2 * (MY_PI/180);
+    double lon1Meters = cos(lat1Radians) * DEGREE_METER_ESTIMATE;
+    double lon2Meters = cos(lat2Radians) * DEGREE_METER_ESTIMATE;
+    return fabs(lon1Meters - lon2Meters);
 }
 
 static void* trackGps() {
@@ -53,9 +57,11 @@ static void* trackGps() {
 
         if (isLocked) {
             double latDist = getDistanceBetweenLats(currGpsVal.latitudeVal, lockedGpsVal.latitudeVal);
-            double lonDist = getDistanceBetweenLons(currGpsVal.longitudeVal, lockedGpsVal.longitudeVal);
+            double lonDist = getDistanceBetweenLons(currGpsVal.latitudeVal, lockedGpsVal.latitudeVal);
+            printf("Distance lat: %f, Distance long: %f\n", latDist, lonDist);
 
-            if (latDist > DIST_THRESHOLD_m || lonDist > DIST_THRESHOLD_m) {
+            if ((latDist > DIST_THRESHOLD_m || lonDist > DIST_THRESHOLD_m) && !isMoved) {
+                printf("Distance lat: %f, Distance long: %f, Turning on Buzzer\n", latDist, lonDist);
                 isMoved = true;
                 Buzzer_alarmOn();
             }
@@ -78,6 +84,7 @@ void GpsTrack_stopTracking() {
 void GpsTrack_lockPosition() {
     isLocked = true;
     lockedGpsVal = currGpsVal;
+    isMoved = false;
     Buzzer_alarmOff();
 }
 
@@ -85,6 +92,7 @@ void GpsTrack_lockPosition() {
 void GpsTrack_unlockPosition() {
     isLocked = false;
     isMoved = false; // Resets isMoved if gps is unlocked
+    Buzzer_alarmOff();
 }
 
 // Returns true if the gps is in lock state but is mov3ed more than 5 meters
